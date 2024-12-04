e492295d222f58855350ba5a72b115fb679ca631
mosesolamide <olamidemoses132@gmail.com>
2024-12-05 00:37:44 +0100

    improved the cart
    improved the cart and added some animation

--- src/cart/Cart.jsx
+++ src/cart/Cart.jsx
@@ -1,7 +1,21 @@
 import React, { useEffect, useState, useContext } from "react"
 import { Quatity } from "../App"
+import { motion } from "framer-motion"
+import "./Cart.css"
 
 export default function Cart(){
+
+  const buttonVariants = {
+    hover: {
+      scale: 1.1,
+      transition: {
+        duration: 0.5,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatType: "reverse",
+      },
+    },
+  }
 
   const {incrementQuatity,decrementQuatity,quatity} = useContext(Quatity)
 
@@ -22,26 +36,53 @@ export default function Cart(){
   }
 
     const cartProduct = cart.length > 0 ? cart.map((carts, index) =>(
-        <article className="products--items mg" key={index}>
-          <img src={`assets/${carts.img}`} alt="images of candles" />
-          <div className="price--name--tag">
-              <p>{`${carts.name}`}</p>
-              <b>{`${carts.price}`}</b>
+      <article className="cart--main" key={index}>
+          <div className="cart--img--name">
+            <img src={`assets/${carts.img}`} alt="img of product" className="carts-product-img" />
+            <p>{`${carts.name}`}</p>
+            <b>{`${carts.price}`}</b>
           </div>
-          <div className="quatity">
+          <div className="remove--cart">
+            <div className="quatity">
               <p onClick={incrementQuatity}>+</p>
               <p>{quatity}</p>
               <p onClick={decrementQuatity}>-</p>
+            </div>
+            <motion.button
+            className="remove"
+            onClick={removeItem}
+            variants={buttonVariants}
+            whileHover="hover" // Trigger the hover animation
+          >
+            Remove
+          </motion.button>
+
           </div>
-          <button className="cart--button remove" onClick={ () => removeItem(carts.id)}>Remove</button>
-    </article>
+          <hr />
+      </article>
   )):<div className="empty">
       <p>Your cart is empty</p>
   </div>
+  
     return (
-      <div className="carts--product">
-        {cartProduct}
-
-      </div>
+      <>
+        <div className="carts--product">
+            {cartProduct}
+        </div>
+      </>
     )
 }
+
+{/* <article className="products--items mg" key={index}>
+<img src={`assets/${carts.img}`} alt="images of candles" />
+<div className="price--name--tag">
+    <p>{`${carts.name}`}</p>
+    <b>{`${carts.price}`}</b>
+</div>
+<div className="quatity">
+    <p onClick={incrementQuatity}>+</p>
+    <p>{quatity}</p>
+    <p onClick={decrementQuatity}>-</p>
+</div>
+<button className="cart--button remove" onClick={ () => removeItem(carts.id)}>Remove</button>
+</article> */}
